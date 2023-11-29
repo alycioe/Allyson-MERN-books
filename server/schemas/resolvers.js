002fbb3133e30893abbf1229b662078f1006c8d3
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-11-28 23:02:01 -0500

    most of the backend is finished, resolvers will need to be touched upon further

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,48 @@
+const { User, Book } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find();
+        },
+
+        user: async (parent, {profileId}) => {
+            return User.findOne({ _id: profileId });
+        },
+
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+            const user = await User.create({ name, email, password });
+            const token = signToken(user);
+
+            return { token, profile };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!profile) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, { profileId, book })
+
+        removeBook: 
+    }
+}
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,45 @@
+const typeDefs = `
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        saveBook(newBook: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
+
+    type BookInput {
+        bookId: String
+        title: String
+        authors: [String]
+        description: String
+        image: String
+        link: String
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+`;
+
+module.exports = typeDefs;
