74ab9b9961696517500b11ecbabaab6ac3fd8c93
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-12-05 22:32:20 -0500

    Trying to fix for heroku

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import App from './src/App.jsx'
-import SearchBooks from './src/pages/SearchBooks.jsx'
-import SavedBooks from './src/pages/SavedBooks.jsx'
+import App from './src/App.jsx';
+import SearchBooks from './src/pages/SearchBooks.jsx';
+import SavedBooks from './src/pages/SavedBooks.jsx';
 
 const router = createBrowserRouter([
   {
