217021af3650a74600c1c11bd70586cb4b297c00
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-12-05 22:23:28 -0500

    make it better

--- server/server.js
+++ server/server.js
@@ -34,8 +34,6 @@ const startApolloServer = async () => {
         });
     }
 
-
-
     db.once('open', () => {
         app.listen(PORT, () => {
             console.log(`API server running on port ${PORT}`);
