df50323294b60f96ca950fe8a380be885008f91e
allysonpacheco@yahoo.com <allysonpacheco@yahoo.com>
2023-12-06 00:44:37 -0500

    fixed some imports

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -11,7 +11,7 @@ import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations';
 
 import Auth from '../utils/auth';
-import { searchGoogleBooks } from '../../MERN-books/src/utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
